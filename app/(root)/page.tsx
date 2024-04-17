e798ec1d33e8234e0aafbb245fc92e6e9ccc72d1
evan <evandromariano49@gmail.com>
2024-04-17 18:55:24 -0300

    first_commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="bg-emerald-400" variant="outline" size="lg">Button</Button>
+    </div>
+
+  );
+}
